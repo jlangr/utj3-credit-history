f37bc26b0ad890d5520c38cf07ae7bbd63c8f242
Jeff L <jeff@langrsoft.com>
2024-05-01 20:09:55 -0600

    var

--- src/test/java/credit/ACreditHistory.java
+++ src/test/java/credit/ACreditHistory.java
@@ -8,7 +8,7 @@ class ACreditHistory {
    @Test
    void withNoCreditRatingsHas0Mean() {
       var creditHistory = new CreditHistory();
-      int result = creditHistory.arithmeticMean();
+      var result = creditHistory.arithmeticMean();
       assertEquals(0, result);
    }
 
@@ -17,7 +17,7 @@ class ACreditHistory {
    void withOneRatingHasEquivalentMean() {
       var creditHistory = new CreditHistory();
       creditHistory.add(new CreditRating(780, LocalDate.now()));
-      int result = creditHistory.arithmeticMean();
+      var result = creditHistory.arithmeticMean();
       assertEquals(780, result);
    }
    // END:breakTheTest
