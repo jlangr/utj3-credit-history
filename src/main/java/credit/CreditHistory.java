1501ee0b58b6baf2a1edbddd664ff99bd0ec6ceb
Jeff L <jeff@langrsoft.com>
2024-05-29 09:04:32 -0600

    isEmpty

--- src/main/java/credit/CreditHistory.java
+++ src/main/java/credit/CreditHistory.java
@@ -11,7 +11,7 @@ public class CreditHistory {
    }
 
    public int arithmeticMean() {
-      if (ratings.size() == 0) return 0;
+      if (ratings.isEmpty()) return 0;
 
       var total = ratings.stream().mapToInt(CreditRating::rating).sum();
       return total / ratings.size();
